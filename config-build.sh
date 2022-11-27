cd81ea7ff8a463e948dabf70e822ee17fe2ff269
Sokratis Poutas <poutasok@gmail.com>
2022-11-27 14:06:41 +0200

    Add multi device matrix mult test

---
+++ config-build.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+# Find the openblas lib:
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/users/spoutas/Libs/OpenBLAS-install/lib
+
+# Configure
+cmake -S . -B build/
+
+# Build
+cmake --build build/
