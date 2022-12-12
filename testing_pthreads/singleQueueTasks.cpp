35fdc5f768e8076af178961a28f25848bf490024
Sokratis Poutas <poutasok@gmail.com>
2022-12-12 15:12:09 +0200

    Add parallelQueues (pthreads) test

--- testing_pthreads/singleQueueTasks.cpp
+++ testing_pthreads/singleQueueTasks.cpp
@@ -3,13 +3,10 @@
 
 // Some dummy task function
 void* taskFun(void* input){
-    int x = * (int*) input;
-    cout << "taskFun: input = " << x << endl;
+    int * x = (int*) input;
+    cout << "taskFun: input = " << *x << endl;
 
-    int sum = 0;
-    for(int i = 0; i < x; i++){
-        sum += i;
-    }
+    *x += 1000;
 
     return 0;
 }
@@ -17,22 +14,19 @@ void* taskFun(void* input){
 int main(){
     CQueue_p MyQueue_p = new CommandQueue(-1);
 
-    int input_1 = 1001;
-    MyQueue_p->add_host_func((void*) &taskFun, (void*) &input_1);
-    int input_2 = 1002;
-    MyQueue_p->add_host_func((void*) &taskFun, (void*) &input_2);
-    int input_3 = 1003;
-    MyQueue_p->add_host_func((void*) &taskFun, (void*) &input_3);
-    int input_4 = 1004;
-    MyQueue_p->add_host_func((void*) &taskFun, (void*) &input_4);
+    int input = 1000;
+    MyQueue_p->add_host_func((void*) &taskFun, (void*) &input);
+    MyQueue_p->add_host_func((void*) &taskFun, (void*) &input);
+    MyQueue_p->add_host_func((void*) &taskFun, (void*) &input);
+    MyQueue_p->add_host_func((void*) &taskFun, (void*) &input);
 
     MyQueue_p->sync_barrier();
     cout << "After sync barrier" << endl;
-    int input_5 = 1005;
-    MyQueue_p->add_host_func((void*) &taskFun, (void*) &input_5);
-    int input_6 = 1006;
-    MyQueue_p->add_host_func((void*) &taskFun, (void*) &input_6);
+    MyQueue_p->add_host_func((void*) &taskFun, (void*) &input);
+    MyQueue_p->add_host_func((void*) &taskFun, (void*) &input);
+    MyQueue_p->sync_barrier();
 
     delete(MyQueue_p);
+    cout << "Main: final result = " << input << endl;
     return 0;
 }
