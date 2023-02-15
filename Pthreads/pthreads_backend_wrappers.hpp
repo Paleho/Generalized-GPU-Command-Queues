c8f9422beb3a0eb0cd1f2a134e7b236f9c45472e
Sokratis Poutas <poutasok@gmail.com>
2023-02-15 23:32:35 +0200

    Configure pthreads and cblas. Add wrappedMatrixMultQueues test

--- Pthreads/pthreads_backend_wrappers.hpp
+++ Pthreads/pthreads_backend_wrappers.hpp
@@ -4,8 +4,11 @@
 /// \brief 
 ///
 
-// #include <unihelpers.hpp>
 #include <pthread.h>
+#include <cblas.h>
+#include "cublas_v2.h"
+#include <unihelpers.hpp>
+
 
 using namespace std;
 
@@ -26,3 +29,34 @@ typedef struct pthread_event {
     pthread_t threadId;
     event_status estate;
 }* pthread_event_p; 
+
+typedef struct gemm_backend_in{
+	char TransA,  TransB;
+	int M, N, K, ldA, ldB, ldC;
+	VALUE_TYPE alpha,beta;
+	void **A, **B, **C;
+	short dev_id;
+}* gemm_backend_in_p;
+
+typedef struct axpy_backend_in{
+	int N, incx, incy;
+	VALUE_TYPE alpha;
+	void **x, **y;
+	short dev_id;
+}* axpy_backend_in_p;
+
+/// Select and run a wrapped operation (e.g. gemm, axpy) depending on opname
+void backend_run_operation(void* backend_data, const char* opname, CQueue_p run_queue);
+
+void cblas_wrap_daxpy(void* backend_data);
+void cblas_wrap_saxpy(void* backend_data);
+void cblas_wrap_dgemm(void* backend_data);
+void cblas_wrap_sgemm(void* backend_data);
+
+void TransposeTranslate(char TransChar, CBLAS_TRANSPOSE* cblasFlag, cublasOperation_t* cuBLASFlag, long int* ldim, long int dim1, long int dim2);
+
+cublasOperation_t OpCblasToCublas(CBLAS_TRANSPOSE src);
+CBLAS_TRANSPOSE OpCublasToCblas(cublasOperation_t src);
+cublasOperation_t OpCharToCublas(char src);
+CBLAS_TRANSPOSE OpCharToCblas(char src);
+char PrintCublasOp(cublasOperation_t src);
