3a76308cdd735bccdb5f41fc07b7306b54ff16b6
Sokratis Poutas <poutasok@gmail.com>
2023-04-05 12:56:17 +0300

    Configure compile and link

--- config-build.sh
+++ config-build.sh
@@ -8,3 +8,6 @@ cmake --build build/
 
 # Find the openblas lib:
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/users/spoutas/Libs/OpenBLAS-install/lib
+
+# Find the hipblas lib:
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rocm-5.4.0/lib
